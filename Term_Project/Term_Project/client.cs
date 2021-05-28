dcd122f0bba4d696eaa1e63f48ccb3eb8e5243ea
carsonjriland <58926650+carsonjriland@users.noreply.github.com>
2021-05-27 17:03:13 -0700

    Update client.cs
    First update of Client code. The next update will start including the disconnecting code and changes made for the use fo client code within the Unity project.

--- Term_Project/Term_Project/client.cs
+++ Term_Project/Term_Project/client.cs
@@ -1,10 +1,80 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System;
 
 namespace Term_ProjectServer
 {
+    class Client
+    {
+        public static int dataBufferSize = 4096;
 
+        public int id;
+        public TcpClient tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP(id);
+        }
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+             
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                receiveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallBack, null);
+
+                //TO-DO: Send welcome packet
+            }
+
+            private void ReceiveCallBack(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if(_byteLength <= 0)
+                    {
+                        //TODO: Disconnect
+                        return;
+                    }
+
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(receiveBuffer, _data, _byteLength);
+
+                    //TODO: Handle
+                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallBack, null);
+                }
+                catch(Exception _ex)
+                {
+                    Console.WriteLine($"Error receiving TCP data: { _ex}");
+                    //TODO: Disconnect
+                   
+                }
+
+                
+
+            }
+
+        }
+
+    }
 }
