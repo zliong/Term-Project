73d63a0eae6535fddd51ca6bea97603252df20f9
Logo251 <61099371+Logo251@users.noreply.github.com>
2021-05-29 17:22:35 -0700

    Updated Server
    Implemented rest of video 1 for server.cs
    KNOWN ISSUE: _client isn't know.

--- Term_Project/Term_Project/server.cs
+++ Term_Project/Term_Project/server.cs
@@ -12,7 +12,7 @@ namespace Term_ProjectServer
       public static int MaxPlayers { get; private set; }
 
       public static int Port { get; private set; }
-
+      public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
       private static TcpListener tcpListener;
 
       public static void Start(int maxPlayers, int port)
@@ -20,17 +20,39 @@ namespace Term_ProjectServer
          maxPlayers = server.MaxPlayers;  //For clarity.
          port = server.Port;              //For clarity.
 
+         Console.WriteLine("Revving up the server!");
+
+         InitalizeServerData();
          tcpListener = new TcpListener(IPAddress.Any, port);
          tcpListener.Start();
          tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
 
-         Console.WriteLine($"Server started on port {port}");
+         Console.WriteLine($"Server started on port {port}!");
       }
 
       private static void TCPConnectCallback(IAsyncResult result)
       {
          TcpClient client = tcpListener.EndAcceptTcpClient(result);
          tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+         Console.WriteLine("Someone is attempting to connect!");
+
+         for(int i = 1; i <= MaxPlayers; i++)
+         {
+            if(clients[i].tcp.Connected == false) //Change from guide for perhaps more efficiency. If something doesn't work check here, may need to be .tcp.socket.
+            {
+               clients[i].tcp.Connect(_client);
+               return;
+            }
+         }
+         Console.WriteLine("Somebody failed to connect: server is full.");
+      }
+
+      private static void InitalizeServerData()
+      {
+         for(int i = 1; i <= MaxPlayers; i++)
+         {
+            clients.Add(i, new Client(i));
+         }
       }
    }
 }
