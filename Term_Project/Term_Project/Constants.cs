cbdd5945c2d9f287666a13824c79a8d4f33bdcf8
KaiELZY <zachary12453@gmail.com>
2021-05-30 22:51:58 -0700

    Added Constants for the server side

---
+++ Term_Project/Term_Project/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Term_ProjectServer
+{
+    class Constants
+    {
+        public const int TICKS_PER_SEC = 60;
+        public const float MS_PER_TICK = 1000f;
+    }
+}
