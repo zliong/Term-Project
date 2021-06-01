41ebe6f18f7833645c0cf190770b05ac0aba89cb
carsonjriland <58926650+carsonjriland@users.noreply.github.com>
2021-05-31 22:23:39 -0700

    Update Packet.cs
    added ReadVector3() and ReadQuaternion()

--- Term_Project/Term_Project/Packet.cs
+++ Term_Project/Term_Project/Packet.cs
@@ -346,6 +346,16 @@ namespace Term_ProjectServer
             throw new Exception("Could not read value of type 'string'!");
          }
       }
+      
+       public Vector3 ReadVector3(bool _moveReadPos = true)
+        {
+            return new Vector3(ReadFloat(_moveReadPos), ReadFloat(_moveReadPos), ReadFloat(_moveReadPos));
+        }
+        public Quaternion ReadQuaternion(bool _moveReadPos = true)
+        {
+            return new Quaternion(ReadFloat(_moveReadPos), ReadFloat(_moveReadPos), ReadFloat(_moveReadPos), ReadFloat(_moveReadPos));
+        }
+        
       #endregion
 
       private bool disposed = false;
