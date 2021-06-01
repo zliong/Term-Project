543b9f465870572e640a3f2dbd3528505c9f33e5
carsonjriland <58926650+carsonjriland@users.noreply.github.com>
2021-05-31 22:18:13 -0700

    Update ServerSend.cs
    added player rotation and position functions

--- Term_Project/Term_Project/ServerSend.cs
+++ Term_Project/Term_Project/ServerSend.cs
@@ -43,6 +43,32 @@ namespace Term_ProjectServer
               SendTCPData(_toClient, _packet);
           }
       }
+      
+         /// <summary>Sends a player's updated position to all clients.</summary>
+        /// <param name="_player">The player whose position to update.</param>
+        public static void PlayerPosition(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerPos))
+            {
+                _packet.Write(_player.player_id);
+                _packet.Write(_player.player_pos);
+
+                SendTCPDataToAllClients(_packet);
+            }
+        }
+
+        /// <summary>Sends a player's updated rotation to all clients except to himself (to avoid overwriting the local player's rotation).</summary>
+        /// <param name="_player">The player whose rotation to update.</param>
+        public static void PlayerRotation(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerRotation))
+            {
+                _packet.Write(_player.player_id);
+                _packet.Write(_player.player_rotation);
+
+                SendTCPDataToAllClientsExcept(_player.player_id, _packet);
+            }
+        }
         //Unsure currently how this is needed, hopefully further guide will explain it.
       public static void SendTCPDataToAllClientsExcept(int client, Packet packet) //Part 2: return of the spicy function names.
       {
